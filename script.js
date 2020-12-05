0047b2ec01f5c3085b3549f196af6cb98a92c034
Kevin Zhou <kevinzhou22@outlook.com>
2020-12-05 17:17:20 +1100

    Create basic operator functions

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+function operate(operator, a , b) {
+    switch (operator) {
+        case "+":
+            return add(a,b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a,b);
+            break;
+        case "/":
+            return divide(a,b);
+            break;
+    }
+}
+
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
