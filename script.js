6add6b1dba48d02f046aef2088c7c96fe6ad82b4
Kevin Zhou <kevinzhou22@outlook.com>
2020-12-06 20:30:13 +1100

    Create styling for display and make other minor UI adjustments

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+/* Adds the focus outline effect if the user starts using tab */
+
+document.body.addEventListener("mousedown", () => document.body.classList.add("using-mouse"));
+document.body.addEventListener("keydown", (e) => {
+    if(e.key === "Tab") {
+        document.body.classList.remove("using-mouse");
+    }
+});
+
+/* Manages the transition when buttons are clicked */
+const buttons = document.querySelectorAll(".button-grid button");
+buttons.forEach((button) => {
+    button.addEventListener("click",(e) => e.currentTarget.classList.add("clicked-button"));
+    button.addEventListener("transitionend", (e) => e.currentTarget.classList.remove("clicked-button"));
+});
+
+
+
+
+
+
+/* takes an operator and two numbers. Returns the outcome of the operation using the two numbers */
 function operate(operator, a , b) {
     switch (operator) {
         case "+":
