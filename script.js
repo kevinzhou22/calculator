30f3398b10078f23b04801d9fb36b63305fc381d
Kevin Zhou <kevinzhou22@outlook.com>
2020-12-07 10:24:11 +1100

    Add basic functionality to number buttons, operator buttons, and the equals sign button

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-/* Adds the focus outline effect if the user starts using tab */
+/***  INITIALIZATION ***/
 
+const display = document.querySelector(".display");
+
+let firstArgument = null;
+let secondArgument = null;
+let currentOperation = "";
+
+const ADD_OPERATOR = "add";
+const SUBSTRACT_OPERATOR = "substract";
+const DIVIDE_OPERATOR = "divide";
+const MULTIPLY_OPERATOR = "multiply";
+
+
+
+/* Adds the focus outline effect if the user starts using tab */
 document.body.addEventListener("mousedown", () => document.body.classList.add("using-mouse"));
 document.body.addEventListener("keydown", (e) => {
-    if(e.key === "Tab") {
+    if (e.key === "Tab") {
         document.body.classList.remove("using-mouse");
     }
 });
@@ -10,29 +24,88 @@ document.body.addEventListener("keydown", (e) => {
 /* Manages the transition when buttons are clicked */
 const buttons = document.querySelectorAll(".button-grid button");
 buttons.forEach((button) => {
-    button.addEventListener("click",(e) => e.currentTarget.classList.add("clicked-button"));
+    button.addEventListener("click", (e) => e.currentTarget.classList.add("clicked-button"));
     button.addEventListener("transitionend", (e) => e.currentTarget.classList.remove("clicked-button"));
 });
 
 
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((button) => button.addEventListener("click", enterNumber));
+const operatorButtons =document.querySelectorAll(".operator");
+operatorButtons.forEach((button)=> button.addEventListener("click",enterOperator))
+const equalsSignButton = document.querySelector(".equals-sign");
+equalsSignButton.addEventListener("click",enterEqualsSign);
+/* 
+Callback function to update the display when a number button is clicked */
+function enterNumber(e) {
+    let displayNumber; 
+    if (secondArgument === null) {
+        displayNumber = e.currentTarget.value;
+    } else {
+        displayNumber = String(secondArgument) + e.currentTarget.value
+    }
 
+    secondArgument = +displayNumber;
+    display.textContent = secondArgument;
+}
+
+/* Callback function to handle operator buttons being clicked */
+function enterOperator(e) {
+    if (firstArgument === null) {
+        firstArgument = secondArgument;
+        currentOperation = determineOperator(e.currentTarget);
+        secondArgument = null;
+    } else {
+        firstArgument = operate(currentOperation,firstArgument,secondArgument);
+        secondArgument = null;
+        currentOperation = determineOperator(e.currentTarget);
+        display.textContent = firstArgument;
+    }
+}
+
+/* Callback function to handle equals sign being clicked */
+function enterEqualsSign(e) {
+    firstArgument = operate(currentOperation,firstArgument,secondArgument);
+    secondArgument = null;
+    currentOperation = "";
+    display.textContent = firstArgument;
+}
+
+/* Takes one argument representing a button node and determines which operator it represents. */
+function determineOperator(button) {
+    let text = button.value;
+    switch (text) {
+        case "add":
+            return ADD_OPERATOR;
+            break;
+        case "subtract":
+            return SUBSTRACT_OPERATOR;
+            break;
+        case "multiply":
+            return MULTIPLY_OPERATOR;
+            break;
+        case "divide":
+            return DIVIDE_OPERATOR;
+            break;
+    }
+}
 
 
 
 /* takes an operator and two numbers. Returns the outcome of the operation using the two numbers */
-function operate(operator, a , b) {
+function operate(operator, a, b) {
     switch (operator) {
-        case "+":
-            return add(a,b);
+        case ADD_OPERATOR:
+            return add(a, b);
             break;
-        case "-":
+        case SUBSTRACT_OPERATOR:
             return subtract(a, b);
             break;
-        case "*":
-            return multiply(a,b);
+        case MULTIPLY_OPERATOR:
+            return multiply(a, b);
             break;
-        case "/":
-            return divide(a,b);
+        case DIVIDE_OPERATOR:
+            return divide(a, b);
             break;
     }
 }
@@ -52,6 +125,6 @@ function multiply(a, b) {
     return a * b;
 }
 
-function divide(a,b) {
+function divide(a, b) {
     return a / b;
 }
